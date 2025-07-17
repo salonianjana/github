8957a3fc535b4dba5e803096c2b79c89f507b41a
salonianjana <salonianjana4@gmail.com>
2025-07-17 09:54:10 +0530

    second.py

---
+++ second.py
@@ -0,0 +1,9 @@
+# Input two digits from the user
+a = int(input("Enter first digit: "))
+b = int(input("Enter second digit: "))
+
+# Calculate the sum
+total = a + b
+
+# Print the result
+print("Sum of two digits is:", total)
