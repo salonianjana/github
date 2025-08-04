1f27039d452c32bb7aee7521f47379fdccb9ab3c
salonianjana <salonianjana4@gmail.com>
2025-08-04 14:42:41 +0530

    condition.py

---
+++ condition.py
@@ -0,0 +1,10 @@
+# Ask the user to enter a number
+num = float(input("Enter a number: "))
+
+# Check conditions using if-elif-else
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
